8fa5072723027e9a3f5458e84b8aedab3ee71315
a.karamiani <aziz.karamiani@gmail.com>
2023-05-21 23:06:16 +0330

    Initial commit

---
+++ src/main/java/com/guru/gurukotlin/GuruKotlinApplication.java
@@ -0,0 +1,13 @@
+package com.guru.gurukotlin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuruKotlinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuruKotlinApplication.class, args);
+    }
+
+}
